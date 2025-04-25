cd610c9fd0545a46cf96de1071944a37ff67ffc9
andy <2768054177@qq.com>
2025-04-25 14:59:35 +0800

    Leetcode33

---
+++ Leetcode33.py
@@ -0,0 +1,48 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums)-1
+
+        while l <= r:
+            mid = (l+r)//2
+
+            if nums[l] <= nums [r]:
+                if nums[mid] == target:
+                    return mid
+                
+                elif nums[mid] < target:
+                    l = mid + 1
+
+                else:
+                    r = mid-1
+            
+            else:
+                # nums[l] > nums[r]
+                if nums[mid] == target:
+                    return mid
+                
+                elif nums[mid] > target:
+                    if nums[mid] <= nums[r]:
+                        r = mid - 1
+                    
+                    elif target >= nums[l]:
+                        r = mid-1
+                    
+                    else :
+                        l = mid+1
+                
+                else:
+                    if nums[mid] > nums[r]:
+                        l = mid+1
+                    
+                    elif target > nums[r]:
+                        r = mid-1
+
+                    else:
+                        l = mid+1
+
+
+        return -1
